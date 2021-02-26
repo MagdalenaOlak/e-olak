42482ae40d58336580561713ecb39364f22195ec
Magdalena Olak <66574001+MagdalenaOlak@users.noreply.github.com>
2021-02-26 19:02:17 +0100

    Add files via upload

---
+++ layout/simplebody-top.php
@@ -0,0 +1,22 @@
+
+
+    <div class="container-fluid">
+
+<div id="fh5co-about-us" data-section="about">
+
+			<div class="row" id="team"  style=" padding-top: 50px;">
+				<div class="col-md-12 section-heading text-center to-animate"><br>
+				<p><a href="index.php" ><span><big><big>O mnie</big></big></span></a></p>
+					
+
+	 		</div>
+
+
+
+
+<div class="col-md-3 text-left to-animate">
+<!------------- Menu lewe (przywileje -------------------->
+
+</div>
+
+<div class="col-md-8 text-left to-animate">
