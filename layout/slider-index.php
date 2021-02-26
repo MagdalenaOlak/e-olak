f5972aab8b18dff505169c448973e4fc68bb67d7
Magdalena Olak <66574001+MagdalenaOlak@users.noreply.github.com>
2021-02-26 19:01:57 +0100

    Add files via upload

---
+++ layout/slider-index.php
@@ -0,0 +1,77 @@
+<div id="slider" data-section="home">
+		<div class="owl-carousel owl-carousel-fullwidth">
+
+		<!-- You may change the background color here.  -->
+		    <div class="item" style="background-image:url(images/slider-index.jpg)">
+		    	<div class="overlay"></div>
+		    	<div class="container" style="position: relative;">
+		    		<div class="row">
+		    			<div class="col-md-8 col-md-offset-2 text-center">
+		    				<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+		    						<h1 class="fh5co-lead to-animate">Testowanie aplikacji</h1>
+									<h2 class="fh5co-sub-lead to-animate"></h3>
+									<p class="to-animate-2"><href="#" data-nav-section="testowanie" class="btn btn-primary btn-lg">Krok po Kroku</a></p>
+									
+								</div>
+							</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+		    
+		    <!-- You may change the background color here.  -->
+		    <div class="item" style="background-image:url(images/magdalena_olak_slider.jpg)">
+		    	<div class="overlay"></div>
+		    	<div class="container" style="position: relative;">
+		    		<div class="row">
+		    			<div class="col-md-8 col-md-offset-2 text-center">
+		    				<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+		    						<h1 class="fh5co-lead to-animate">Kontakt</h1>
+									<h2 class="fh5co-sub-lead to-animate">Magdalena Olak - jeśli szukasz cierpliwego testera</h3>
+									<p class="to-animate-2"><a href="#" data-nav-section="kontakt" class="btn btn-primary btn-lg">Zapraszam do kontaktu</a></p>
+
+								</div>
+							</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+
+
+
+
+		<!-- You may change the background color here.  -->
+		    <div class="item" style="background-image:url(images/testowanie.jpg)">
+		    	<div class="overlay"></div>
+		    	<div class="container" style="position: relative;">
+		    		<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center">
+					<!-- <div class="col-md-7 col-md-push-1 col-md-push-5 col-sm-7 col-sm-push-1 col-sm-push-5 text-center"> -->
+			    			<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+						    		<h1 class="fh5co-lead to-animate">Na drodze ISTQB</h1>
+									<h2 class="fh5co-sub-lead to-animate">Droga do idealnej pracy</h3>
+									<p class="to-animate-2"><a href="testowanie_krok_po_kroku.php" class="btn btn-primary btn-lg">Krok po kroku</a></p>
+						    	</div>
+						    </div>
+					</div>
+
+
+
+		    		</div>
+		    	</div>
+		    </div>
+
+
+
+			
+
+
+
+
+
+
+		</div>
+	</div>
