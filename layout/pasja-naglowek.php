0e7124475f9de0d7c3685c7a52109fb743743fa2
Magdalena Olak <66574001+MagdalenaOlak@users.noreply.github.com>
2021-02-26 19:02:50 +0100

    Add files via upload

---
+++ layout/pasja-naglowek.php
@@ -0,0 +1,15 @@
+<a name="korzysci"></a>
+<div id="fh5co-about-us" data-section="about">
+                <div class="container">
+			<div class="row" id="team"  style=" padding-top: 50px;">
+				<div class="col-md-10 col-md-offset-2 section-heading text-center to-animate">
+					<h2><a href="przywileje_klubu_oriflame.php" ><span>Testowanie to nie praca</span></a></h2><p></p>
+					<p><a  style=" color: #CCCCCC;"class="dymek" href="#">(Czcionka +/-)<span>Jeśli chcesz zmienić wielkość tekstu, naciśnij na klawiaturze CTRL oraz plus (+) lub minus (-)</span></a></p>
+	 			</div>
+
+<div class="col-md-2 text-left to-animate">
+<!------------- Menu lewe (przywileje -------------------->
+<?php include('layout/menu-testowanie.php'); ?>
+</div>
+
+<div class="col-md-10 text-left to-animate">
