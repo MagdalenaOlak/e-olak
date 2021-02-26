a6dbd60316d33a51a57c246e16c2efbbf752d16d
Magdalena Olak <66574001+MagdalenaOlak@users.noreply.github.com>
2021-02-26 19:01:26 +0100

    Add files via upload

---
+++ layout/slider-page.php
@@ -0,0 +1,77 @@
+
+	<div id="slider" data-section="home">
+		<div class="owl-carousel owl-carousel-fullwidth">
+
+		<!-- You may change the background color here.  -->
+		    <div class="item" style="background-image:url(images/katalog.jpg)">
+		    	<div class="overlay"></div>
+		    	<div class="container" style="position: relative;">
+		    		<div class="row">
+		    			<div class="col-md-8 col-md-offset-2 text-center">
+		    				<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+		    						<h1 class="fh5co-lead to-animate">Aktualny katalog Oriflame</h1>
+									<h2 class="fh5co-sub-lead to-animate">Czyli najnowsze promocje i oferty.  </h3>
+									<p class="to-animate-2"><a href="katalog.html" class="btn btn-primary btn-lg">Obejrzyj</a></p>
+								</div>
+							</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+
+
+		<!-- You may change the background color here.  -->
+		    <div class="item" style="background-image:url(images/pw_2.jpg)">
+		    	<div class="overlay"></div>
+		    	<div class="container" style="position: relative;">
+		    		<div class="row">
+					<div class="col-md-8 col-md-offset-2 text-center">
+					<!-- <div class="col-md-7 col-md-push-1 col-md-push-5 col-sm-7 col-sm-push-1 col-sm-push-5 text-center"> -->
+			    			<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+						    		<h1 class="fh5co-lead to-animate">Program Witamy</h1>
+									<h2 class="fh5co-sub-lead to-animate">Czyli nagrody dla nowych Członków Klubu o wartości przekraczającej 600 zł.  </h3>
+									<p class="to-animate-2"><a href="program_witamy.html" class="btn btn-primary btn-lg">Zobacz nagrody</a></p>
+						    	</div>
+						    </div>
+					</div>
+
+
+
+		    		</div>
+		    	</div>
+		    </div>
+
+
+
+			<!-- You may change the background color here.  -->
+		    <div class="item" style="background-image:url(images/pw-2a.jpg)">
+		    	<div class="overlay"></div>
+		    	<div class="container" style="position: relative;">
+		    		<div class="row">
+		    			<div class="col-md-8 col-md-offset-2 text-center">
+		    				<div class="fh5co-owl-text-wrap">
+						    	<div class="fh5co-owl-text">
+		    						<h1 class="fh5co-lead to-animate">20% taniej niż w katalogu</h1>
+									<h2 class="fh5co-sub-lead to-animate">Dołącz do Klubu Oriflame i&nbsp;kupuj dla siebie o 20 % taniej  niż w katalogu. Wystarczy standarowa <a href="#" data-nav-section="press"><span>rejestracja</span></a></h3>
+									<p class="to-animate-2"><a href="20_taniej.html" class="btn btn-primary btn-lg">wiecej szczegółów</a></p>
+								</div>
+							</div>
+		    			</div>
+		    		</div>
+		    	</div>
+		    </div>
+
+
+
+
+
+
+		</div>
+	</div>
+	
+	
+	
+	
+	
---
+++ layout/stopka.php
@@ -0,0 +1,84 @@
+
+
+<footer id="footer" role="contentinfo">
+		<div class="container">
+			<div class="row row-bottom-padded-sm">
+				<div class="col-md-12">
+					<p class="copyright text-center">Copyright 2020 &copy;  <a href="index.php">e-olak.pl</a>. All Rights Reserved. <a href="2-cv.php" target="_blank">O mnie</a>
+                                        <br>Designed by <a href="http://e-olak.pl/" target="_blank">e-Olak.pl</a> | View  <a href="polityka_prywatnosci.php#polityka" target="_blank">Polityka prywatności</a></p>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-12 text-center">
+					<ul class="social social-circle">
+						<li><a href="https://www.facebook.com/magdalena.olak.3"><i class="icon-facebook"></i></a></li>
+                                                <!-----   TRESC ukryta
+						<li><a href="https://www.youtube.com/watch?v=WglMt3D44uo"><i class="icon-youtube"></i></a></li>
+                                                <li><a href="http://www.ori-network.pl"><i class="icon-esc"></i></a></li>
+						<li><a href="http://e-olak.pl"><i class="icon-cogs"></i></a></li>
+						<li><a href="https://www.facebook.com/OriNetworkPL/"><i class="icon-facebook-square"></i></a></li>
+					------>
+
+                                               
+                                               
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+
+
+<!-- jQuery -->
+	<script src="js/jquery.min.js"></script>
+	<!-- jQuery Easing -->
+	<script src="js/jquery.easing.1.3.js"></script>
+	<!-- Bootstrap -->
+	<script src="js/bootstrap.min.js"></script>
+	<!-- Waypoints -->
+	<script src="js/jquery.waypoints.min.js"></script>
+	<!-- Owl Carousel -->
+	<script src="js/owl.carousel.min.js"></script>
+
+	<!-- For demo purposes only styleswitcher ( You may delete this anytime ) -->
+	<script src="js/jquery.style.switcher.js"></script>
+	<script>
+	$(function(){
+		$('#colour-variations ul').styleSwitcher({
+			defaultThemeId: 'theme-switch',
+			hasPreview: false,
+			cookie: {
+	          	expires: 30,
+	          	isManagingLoad: true
+	      	}
+		});
+		$('.option-toggle').click(function() {
+			$('#colour-variations').toggleClass('sleep');
+		});
+	});
+	</script>
+
+
+
+        <!-- Image hover -->
+        <script type="text/javascript">
+
+        function Obrazki(nazwa){
+
+        oknoObrazki=window.open(nazwa, 'oknoObr', 'width=1600,height=1000')
+
+        oknoObrazki.focus()
+
+        }
+         </script>
+
+
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/main.js"></script>
+
+	<!-- Main JS (Do not remove) -->
+	<script src="js/custom-main.js"></script>
+
+	</body>
+</html>
