6749fa3499fdbbaf4013cd2561a3f5e01844533b
Magdalena Olak <66574001+MagdalenaOlak@users.noreply.github.com>
2021-03-05 10:23:40 +0100

    Update

--- layout/slider-index.php
+++ layout/slider-index.php
@@ -11,15 +11,15 @@
 						    	<div class="fh5co-owl-text">
 		    						<h1 class="fh5co-lead to-animate">Testowanie aplikacji</h1>
 									<h2 class="fh5co-sub-lead to-animate"></h3>
-									<p class="to-animate-2"><href="#" data-nav-section="testowanie" class="btn btn-primary btn-lg">Krok po Kroku</a></p>
-									
+									<p class="to-animate-2"><a href="#" data-nav-section="testowanie" class="btn btn-primary btn-lg">Krok po Kroku</a></p>
+
 								</div>
 							</div>
 		    			</div>
 		    		</div>
 		    	</div>
 		    </div>
-		    
+
 		    <!-- You may change the background color here.  -->
 		    <div class="item" style="background-image:url(images/magdalena_olak_slider.jpg)">
 		    	<div class="overlay"></div>
@@ -66,7 +66,7 @@
 
 
 
-			
+
 
 
 
