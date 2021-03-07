bfa158ec25ae6a01edb8e25af6455e60bf1b464d
Magdalena Olak <66574001+MagdalenaOlak@users.noreply.github.com>
2021-03-07 18:13:43 +0100

    init

---
+++ layout/menu-praca.php
@@ -0,0 +1,66 @@
+
+<ul class="nav"  style=" line-height:10px; ">
+
+
+                            <li><a href="praca_konsultantka_oriflame_praca.php">Konsultant Oriflame</a></li>
+		            
+		            <li><a href="praca_konsultantka_oriflame_praca-biznes_krok_po_kroku.php" ><span><h3>Praca krok po kroku</h3></span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-poznaj_oriflame.php" ><span><h4>§ I    Poznaj Oriflame:</h4></span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-pelne_czlonkostwo_w_klubie_oriflame.php" ><span>Pełne członkostwo w Klubie</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-twój_czas_to_pieniadz.php" ><span>Twój czas to pieniądz</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-plan_marketingowy.php" ><span>Plan Marketingowy - podstawowy dochód</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-plan_marketingowy_dochod_pasywny.php" ><span>Plan Marketingowy - dochód pasywny</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-przeszkody_w_osiagnieciu_sukcesu.php" ><span>Przeszkody - praca nie dla wszystkich</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-faq.php" ><span>FAQ</span></a></li>
+
+                            <li><a href="praca_konsultantka_oriflame_praca-od_czego_zaczac_zarabianie_pieniedzy.php" ><span><h4>§ II   Od czego zacząć zarabianie:</h4></span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-prawo_do_odbierania_nagrod_pienieznych.php" ><span>Uzyskaj prawo do odbierania nagród pieniężnych</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-zapros_znajomych_jak_zarejestrowac_nowego_konsultanta.php" ><span>Zaproś znajomych do Klubu Oriflame - czyli...</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-pierwsze_kroki_z_nowym_konsultantem.php" ><spann  style=" color: 	#e5ecf2;">Pierwsze kroki z nowym konsultantem</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-osiagnij_poziom_menadzerski.php" ><spann  style=" color: #e5ecf2;">Osiągnij poziom menadżerski</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-wykreuj_3_menadzerow.php" ><spann  style=" color: #e5ecf2;">Wykreuj 3 mendżerów</span></a></li>
+
+		            <li><a href="praca_konsultantka_oriflame_praca-biznes_przez_internet.php" ><span><h4n  style=" color: #e5ecf2;">§ III    Biznes przez Internet</span></h4></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-zacznij_zarabiac_oriflame_ori-network.php" ><spann>Zacznij zarabiać z Oriflame & Ori-Network</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-twoj_artykul_na_ori-network.php" ><spann  style=" color: #e5ecf2;">Twój artykuł na Ori-Network</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-twoja_strona_www_gratis.php" ><spann  style=" color: #e5ecf2;">Twoja strona WWW gratis</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-e-booki.php" ><spann  style=" color: #e5ecf2;">E-booki</a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-ogloszenie_w_internecie.php" ><spann  style=" color: #e5ecf2;">Ogłoszenie w Internecie</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-blog_towja_wizytowka.php" ><spann  style=" color: #e5ecf2;">Blog - Twoja wizytówka</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-portale_spolecznosciowe.php" ><spann  style=" color: #e5ecf2;">Portale społecznościowe</span></a></li>
+		            <li><a href="praca_konsultantka_oriflame_praca-.php" ><span></span></a></li>
+		            
+		            <span  style=" color: Gray;"></span>
+		     
+
+		            
+		            
+		            
+		            <li><a href="praca_konsultant_oriflame_baza_wiedzy.php" ><span><h4>§ IV BAZA WIEDZY</h4></span></a></li>
+		            
+		            <li></li>
+			    <li><a href="o_oriflame.php">O Oriflame</a></li>
+			    <li><a href="o_mnie-kariera_w_oriflame.php">O mnie</a></li>
+                            <li><a href="praca_konsultantka_oriflame_praca-rejestracja.php" ><span>Rejestracja on-line</span></a></li>
+                            <li></li>
+
+<!------------------------------------------------------------------
+              
+			    
+			    
+			    
+			    
+			    
+			    <li><a href="praca_konsultantka_oriflame_praca-pierwsze_kroki_z_nowym_konsultantem_online.php" ><span>Pierwsze kroki z nowym konsultantem</span></a></li>
+			    
+			    <li><a href="praca_konsultantka_oriflame_praca-menadzer_klubu_oriflame.php" ><span>Poziom menadżerski</span></a></li>
+			    
+			    <li></li>
+			    <li></li>
+			    <li></li>
+			    <li></li>
+
+ Początek zawartości strony
+------------------------------------------------------------------->
+
+                          </ul>
